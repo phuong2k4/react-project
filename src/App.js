2680db753301754928830b38f1636e984b755f71
phuong2k4 <phuongsepfg@gmail.com>
2024-09-20 21:17:45 +0700

    Challenge 4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import React from 'react';
 // import AlbumsMusic from './features/Album/index.jsx';
 // import TodoFeatures from './features/Todo/index.jsx';
-// import UseImmerIntroduce from './features/UseImmer/component/hookImmer.jsx';
-import ContactManager from './features/Preserving and Resetting State/Challenge_3/Challenge3.jsx';
+import Gallery from './features/Preserving and Resetting State/Challenge_4/Challenge4.jsx';
 
 function App() {
   return (
     <div className="App">
-      <ContactManager/>
+      <Gallery/>
     </div>
   );
 }
