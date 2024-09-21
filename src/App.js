8de292182fed495e526351f541d6d36373cd86cd
phuong2k4 <phuongsepfg@gmail.com>
2024-09-21 22:57:12 +0700

    challenge 5

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import React from 'react';
 // import AlbumsMusic from './features/Album/index.jsx';
 // import TodoFeatures from './features/Todo/index.jsx';
-import Gallery from './features/Preserving and Resetting State/Challenge_4/Challenge4.jsx';
+import ContactListEmail from './features/Preserving and Resetting State/Challenge_5/Challenge5.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <Gallery/>
+      <ContactListEmail />
     </div>
   );
 }
